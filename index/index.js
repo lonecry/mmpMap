8bb6b36e55e0ee07d84c7a536a8fb3adb2bb5de2
大森朝夕 <869490494@qq.com>
2019-05-20 17:40:15 +0800

    init

---
+++ index/index.js
@@ -0,0 +1,45 @@
+const app = getApp()
+Page({
+	data          : {
+		latitude  : 31.259616,
+		longitude : 100.13026,
+		// latitude  : 30.1533191645,
+		// longitude : 120.2591371536,
+		markers   : [
+			{
+				id       : 1,
+				latitude : 30.1533191645,
+				longitude: 120.2591371536,
+				name     : 'T.I.T 创意园'
+			}
+		],
+		covers    : [
+			{
+				latitude : 30.1533191645,
+				longitude: 120.2591371536,
+				iconPath : '/image/location.png'
+			},
+		],
+		permission: false
+	},
+	onReady       : function (e){
+		this.mapCtx = wx.createMapContext('myMap')
+	},
+	onShow        : function (){
+		var _this = this;
+	},
+	moveToLocation: function (){
+		this.mapCtx.moveToLocation()
+	},
+	gogogo: function (){
+		wx.vibrateShort()
+		var _this = this
+		wx.openLocation({ //所以这里会显示你当前的位置
+			latitude : 30.1533191645,
+			longitude: 120.2591371536,
+			name     : "杭州市",
+			address  : "萧山崇化",
+			scale    : 28
+		})
+	},
+})
