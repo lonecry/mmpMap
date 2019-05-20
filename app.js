5689c305ac1f949fb39daf5b2989b762164637a1
home <869490494@qq.com>
2019-05-20 21:18:16 +0800

    init

--- app.js
+++ app.js
@@ -1,4 +1,75 @@
 App({
-	onLaunch: function (){
-	},
+    onLaunch: function (){
+    },  //获取用户地理位置权限
+    getPermission: function (obj){
+        var pmse = new Promise((resolve, reject) =>{
+            wx.getLocation({
+                type: 'wgs84', //返回可以用于wx.openLocation的经纬度
+                success: function (res){
+                    console.log(res);
+                    obj.setData({
+                        latitude: res.latitude,     //latitude,
+                        longitude: res.longitude      //latitude
+                    })
+                    resolve()
+                },
+                fail: function (){
+                    wx.getSetting({
+                        success: function (res){
+                            var statu = res.authSetting;
+                            if (!statu['scope.userLocation']) {
+                                wx.showModal({
+                                    title: '是否授权当前位置',
+                                    content: '需要获取您的地理位置，请确认授权，否则地图功能将无法使用',
+                                    success: function (tip){
+                                        if (tip.confirm) {
+                                            wx.openSetting({
+                                                success: function (data){
+                                                    if (data.authSetting["scope.userLocation"] === true) {
+                                                        wx.showToast({
+                                                            title: '授权成功',
+                                                            icon: 'success',
+                                                            duration: 1000
+                                                        })
+                                                        //授权成功之后，再调用chooseLocation选择地方
+                                                        wx.getLocation({
+                                                            type: 'wgs84', //返回可以用于wx.openLocation的经纬度
+                                                            success: function (res){
+                                                                console.log(res);
+                                                                obj.setData({
+                                                                    latitude: res.latitude,     //latitude,
+                                                                    longitude: res.longitude      //latitude
+                                                                })
+                                                                resolve()
+                                                            },
+                                                        })
+                                                    } else {
+                                                        wx.showToast({
+                                                            title: '授权失败',
+                                                            icon: 'success',
+                                                            duration: 1000
+                                                        })
+                                                        reject()
+                                                    }
+                                                }
+                                            })
+                                        }
+                                    }
+                                })
+                            }
+                        },
+                        fail: function (res){
+                            wx.showToast({
+                                title: '调用授权窗口失败',
+                                icon: 'success',
+                                duration: 1000
+                            })
+                            reject()
+                        }
+                    })
+                }
+            })
+        })
+        return pmse
+    }
 })
--- index/index.js
+++ index/index.js
@@ -1,45 +1,50 @@
 const app = getApp()
 Page({
-	data          : {
-		latitude  : 31.259616,
-		longitude : 100.13026,
-		// latitude  : 30.1533191645,
-		// longitude : 120.2591371536,
-		markers   : [
-			{
-				id       : 1,
-				latitude : 30.1533191645,
-				longitude: 120.2591371536,
-				name     : 'T.I.T 创意园'
-			}
-		],
-		covers    : [
-			{
-				latitude : 30.1533191645,
-				longitude: 120.2591371536,
-				iconPath : '/image/location.png'
-			},
-		],
-		permission: false
-	},
-	onReady       : function (e){
-		this.mapCtx = wx.createMapContext('myMap')
-	},
-	onShow        : function (){
-		var _this = this;
-	},
-	moveToLocation: function (){
-		this.mapCtx.moveToLocation()
-	},
-	gogogo: function (){
-		wx.vibrateShort()
-		var _this = this
-		wx.openLocation({ //所以这里会显示你当前的位置
-			latitude : 30.1533191645,
-			longitude: 120.2591371536,
-			name     : "杭州市",
-			address  : "萧山崇化",
-			scale    : 28
-		})
-	},
+    data: {
+        // latitude  : 31.259616,
+        // longitude : 100.13026,
+        latitude: 30.1533191645,
+        longitude: 120.2591371536,
+        addr: '请选择位置',
+        markers: [
+            {
+                id: 1,
+                latitude: 30.1533191645,
+                longitude: 120.2591371536,
+                name: 'T.I.T 创意园'
+            }
+        ],
+        covers: [
+            {
+                latitude: 30.1533191645,
+                longitude: 120.2591371536,
+                iconPath: '/image/location.png'
+            },
+        ],
+        permission: false
+    },
+    onReady: function (e){
+        this.mapCtx = wx.createMapContext('myMap')
+    },
+    onShow: function (){
+        var _this = this;
+    },
+    moveToLocation: function (){
+        this.mapCtx.moveToLocation()
+    },
+    gogogo: function (){
+        // wx.vibrateShort()
+        var _this = this
+        app.getPermission(_this).then(//传入that值可以在app.js页面直接设置内容
+           function (_this){
+               wx.openLocation({ //所以这里会显示你当前的位置
+                   latitude: 30.1533191645,
+                   longitude: 120.2591371536,
+                   name: "杭州市",
+                   address: "萧山崇化",
+                   scale: 28
+               })
+           }
+        );
+    },
 })
